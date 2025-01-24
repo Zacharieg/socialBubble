72396276efadae4cfeca7609deb335d149e020fb
Zacharie Guet <guetzac@gmail.com>
2025-01-24 21:59:38 +0100

    initialize ennemies and character scripts

---
+++ scripts/character.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name Character
+
+const MAX_HIT_POINT = 1
+
+@onready var shield : Shield = $shield
---
+++ scripts/ennemy.gd
@@ -0,0 +1,8 @@
+extends Node2D
+class_name Ennemy
+
+func spawn():
+	pass
+
+func die():
+	pass
