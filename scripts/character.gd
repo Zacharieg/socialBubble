8c5794398b3cd6dedc0a10077215d3eacb416fcc
Zacharie Guet <guetzac@gmail.com>
2025-01-25 09:58:02 +0100

    resolve merge conflicts

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,9 +1,28 @@
-extends Node2D
+extends Area2D
 class_name Character
 
 const MAX_HIT_POINT = 1
 
-@onready var shield : Shield = $shield
-
 func ready():
 	position = get_viewport_rect().size / 2
+
+@onready var shield : Shield = $shield
+
+@onready var hurtBox : CollisionShape2D = $hurtBox
+
+func _ready() -> void:
+	hurtBox.shape = CircleShape2D.new()
+	hurtBox.shape.radius = shield.SHIELD_RADIUS - shield.SHIELD_THICKNESS/2
+
+func hit_ennemy(ennemy : Area2D):
+	var ennemy_angle = global_position.angle_to(ennemy.global_position)
+	if ennemy_angle < shield.get_start_angle() or ennemy_angle > shield.get_end_angle():
+		hurt()
+
+func hurt():
+	pass
+
+func _on_area_entered(area: Area2D) -> void:
+	#if area is Ennemy:
+		#hit_ennemy(ennemy)
+	hit_ennemy(area)
