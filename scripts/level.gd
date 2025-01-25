f7b30661d9f653dc411846341dff99c799aa292c
jordanlis <68544949+jordanlis@users.noreply.github.com>
2025-01-25 15:22:20 +0100

    Difficulty tweak
    Frequency and angle are increasing between each day.

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 var day_timer: Timer
 var current_day = 1
-var difficulty_frequency_diminishing = 0.5
-const DAYS_DURATION = 5.0
+var difficulty_frequency_diminishing = 0.2
+var difficulty_increase_angle = 10
+const DAYS_DURATION = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,11 +16,18 @@ func _ready() -> void:
 	
 	
 func new_day():
-	print("the day starts")
+	#print("the day starts")
 	#print("Current day : ", current_day)
 	get_tree().get_root().get_node("game/ui/Label").text = str("Jour n° ", current_day)
+	
+	#Les ennemis sont générés de plus en plus fréquemment
 	get_tree().get_root().get_node("game/ennemy_spawner").inbetween_spawning_time -= difficulty_frequency_diminishing
 	get_tree().get_root().get_node("game/ennemy_spawner").set_spawning_timer()
+	
+	#L'angle de génération des ennemis est de plus en plus élevé
+	if get_tree().get_root().get_node("game/ennemy_spawner").difficulty_angle_max + difficulty_increase_angle <= 359:
+		get_tree().get_root().get_node("game/ennemy_spawner").difficulty_angle_max += difficulty_increase_angle
+	else : get_tree().get_root().get_node("game/ennemy_spawner").difficulty_angle_max = 359
 	set_day_timer()
 
 func set_day_timer():
