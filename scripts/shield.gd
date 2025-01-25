1f7c7ffbe3b81dddbffab044668f5397823c1f18
Zacharie Guet <guetzac@gmail.com>
2025-01-25 09:58:02 +0100

    resolve merge conflicts

--- scripts/shield.gd
+++ scripts/shield.gd
@@ -1,5 +1,46 @@
 extends Node2D
 class_name Shield
 
-func move():
-	pass
+const SHIELD_RADIUS : float = 100
+const SHIELD_INITIAL_SIZE : float = 0.2
+const SHIELD_THICKNESS : float = 10
+const SHIELD_INITIAL_SPEED : float = 1
+
+var shield_speed : float = SHIELD_INITIAL_SPEED
+var shield_size : float = SHIELD_INITIAL_SIZE # from 0 to 1
+var shield_position : float = 0 # from 0 to 1
+
+func _draw():
+	print(get_start_angle())
+	draw_arc(
+		Vector2.ZERO,
+		SHIELD_RADIUS,
+		get_start_angle(),
+		get_end_angle(),
+		100,
+		Color.WHITE,
+		10
+	)
+
+func _process(delta: float) -> void:
+	var direction = 0
+	if Input.is_action_pressed("left"):
+		direction -= 1
+	if Input.is_action_pressed("right"):
+		direction += 1
+	if direction != 0:
+		move(direction, delta)
+		queue_redraw()
+
+func move(direction : int, delta : float): # direction is 1 or -1
+	shield_position += direction * shield_speed * delta
+	while shield_position > 1:
+		shield_position -= 1
+	while shield_position < 0:
+		shield_position += 1
+
+func get_start_angle():
+	return 2 * PI * shield_position
+
+func get_end_angle():
+	return 2 * PI * shield_position + shield_size * 2 * PI
