1d9a07559b8aacf8e5fc1dcc17d038b9335a2ede
Zacharie Guet <guetzac@gmail.com>
2025-01-25 12:39:41 +0100

    adding perferct impacts

--- scripts/character.gd
+++ scripts/character.gd
@@ -8,15 +8,32 @@ const HIT_WINDOW = 0.2
 
 @onready var hurtBox : CollisionShape2D = $hurtBox
 
+var time_since_action : float = 100.
+
 func _ready() -> void:
 	#position = get_viewport_rect().size / 2.
 	hurtBox.shape = CircleShape2D.new()
 	hurtBox.shape.radius = shield.SHIELD_RADIUS - shield.SHIELD_THICKNESS/2
 
+func _process(delta: float) -> void:
+	time_since_action += delta
+	if Input.is_action_pressed("action"):
+		time_since_action = 0.
+
 func hit_ennemy(ennemy : Ennemy):
-	ennemy.die()
-	if not shield.is_ennemy_on_shields(ennemy):
+	ennemy.dead = true
+	await get_tree().create_timer(HIT_WINDOW/2).timeout
+	var perfect_stop = false
+	
+	if shield.is_ennemy_on_shields(ennemy):
+		perfect_stop = time_since_action < HIT_WINDOW
+		stop_perfect()
+	else :
 		hurt()
+	ennemy.die(perfect_stop)
+
+func stop_perfect():
+	pass
 
 func hurt():
 	#get_tree().quit()
