bb8cecbabe1d42a6f818657a7d9b3faf9306434e
jordanlis <68544949+jordanlis@users.noreply.github.com>
2025-01-31 13:16:02 +0100

    Toggle fullscreen with alt enter

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func _input(event):
+	if event.is_action_pressed("fullscreen"):
+		toggle_fullscreen()
+
+func toggle_fullscreen():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
