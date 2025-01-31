5fb5b34704947eafd40595614da497043cd6a329
jordanlis <68544949+jordanlis@users.noreply.github.com>
2025-01-31 14:15:14 +0100

    Tuto before intro animation

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,7 +1,21 @@
 extends Node2D
 
-var intro_is_passed = false
+var tuto_is_passed = false
+@onready var tuto = $tuto
+@onready var animation_player = $AnimationPlayer
 
-func _input(event) :
-	if event.is_pressed():
-		get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _input(event):
+	if event.is_action_pressed("action"):
+		if not tuto_is_passed:
+			hide_tutorial()
+		elif tuto_is_passed:
+			start_game()
+
+func hide_tutorial():
+	tuto.queue_free()
+	animation_player.play("idle")
+	tuto_is_passed = true
+
+func start_game():
+	var next_scene = preload("res://scenes/game.tscn")
+	get_tree().change_scene_to_packed(next_scene)
